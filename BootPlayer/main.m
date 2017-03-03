bb08cf2c7cf38b0a59290e4be5ec84d2c38a7dad
wangzhenchao <wangzc@msgcopy>
2017-03-03 10:20:14 +0800

    Initial Commit

---
+++ BootPlayer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BootPlayer
+//
+//  Created by sh on 2017/3/3.
+//  Copyright © 2017年 WZC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
